4e8bb122c722de20cb36decf13099aaea14934b5
rainara Silva <rainara.cps@gmail.com>
2022-07-25 23:13:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/* VAriaável do tipo string */
+var nome = 'Rainara Silva';
+var email = 'rainara.cps@gmail.com';
+/* Variável do tipo INT */
+var idade = 20;
+var salário = 2.045;
+/* Variável do tipo decimal */
+var salário2 = 2.589;
+var salario_total = salário + salário2;
+
+var MediaSalarial = (salario_total/2);
+
+/* Mostrando a variável nome */
+
+console.log('Olá, '+nome +' seu email é: '+ email);
+console.log('Sua idade é '+ idade +' anos.');
+console.log('Seu primeiro salário é '+ salário);
+console.log('Seu segundo salário é '+ salário2);
+console.log('Seu salário total é '+ salario_total+ ' reais.' );
+console.log('Sua média salarial é de: ' + MediaSalarial);
+
+//nome = 'Rainara';
+//console.log(nome);
+
+/*
+console.log(email);
+console.log(idade);
+console.log(salário);
+console.log(salário2);
+*/
